a3ff4949d3f57e0247be1083eb60061d4d595d0d
Rayner <raynersulyak@gmail.com>
2018-12-04 22:32:20 -0200

    added loss and win

--- minesweeper2/main.py
+++ minesweeper2/main.py
@@ -11,6 +11,7 @@ WIDTH = 400
 FPS = 15
 
 NUM_MINES = 30
+MAX_MINAS = (WIDTH // 20) * (HEIGHT // 20)
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,9 +23,15 @@ M_RCLICK = 3
 
 pg.font.init()
 my_font = pg.font.SysFont('Arial', 30)
-text = my_font.render("", True, [126, 126, 126])
+PERDEU_TEXT = my_font.render("Tu perdeu", True, [126, 126, 126])
+GANHOU_TEXT = my_font.render("Tu ganhou", True, [126, 126, 126])
 
 def main():
+    # n pode haver mais minas q quadrados
+    if(NUM_MINES > MAX_MINAS):
+        print("Tem minas demais!")
+        sys.exit(-1)
+    
     print("path:", PATH)
     # init
     pg.init()
@@ -46,30 +53,18 @@ def main():
     sprites.add(mine_field.fields)
 
 
+    # contador de bandeiras
+    count_flags = 0
+    # conta as bandeiras q realmente indicam uma bomba
+    count_actual_flags = 0
 
     perdeu = False
     won = False
-    while (not perdeu and not won) or True :
+    while True:
         for event in pg.event.get():
             if(event.type == pg.QUIT):
                 pg.quit()
                 sys.exit()
-            if(perdeu or won):
-                continue
-            if(event.type == pg.MOUSEBUTTONDOWN):
-                x, y = event.pos
-                for minas in mine_field.fields:
-                    for mina in minas:
-                        if(mina.rect.collidepoint(x, y)):
-                            if(event.button == M_RCLICK):
-                                mina.on_right_click()
-                            elif(event.button == M_LCLICK):
-                                #mina.on_left_click()
-                                if(mina.number == Field.BOMB and mina.hiden == Field.DEFAULT):
-                                    perdeu = True
-                                    mine_field.revelar()
-                                    mina.set_bomb_exploded()
-                                mine_field.flood_fill(mina)
             if(event.type == pg.KEYDOWN):
                 if(event.key == pg.K_F2):
                     # def reset()
@@ -78,21 +73,61 @@ def main():
                     sprites.add(mine_field.fields)
                     mine_field.get_mines(NUM_MINES)
                     mine_field.get_numbers()
+                    count_actual_flags = 0
+                    count_flags = 0
                     perdeu = False
                     won = False
                 if(event.key == pg.K_F1):
                     perdeu = True
-                                      
+            if(perdeu or won):
+                continue
+            # quando clicka num quadrado
+            if(event.type == pg.MOUSEBUTTONDOWN):
+                x, y = event.pos
+                # passa por todas as minas procurando a bomba dentro de x, y
+                for minas in mine_field.fields:
+                    for mina in minas:
+                        if(mina.rect.collidepoint(x, y)):
+
+                            # botao direito
+                            if(event.button == M_RCLICK):
+                                mina.on_right_click()
+
+                                # virou flag
+                                if(mina.hiden == Field.FLAG):
+                                    count_flags += 1
+                                    if(mina.number == Field.BOMB):
+                                        count_actual_flags += 1
+                                # saiu de flag
+                                    # lembrando q se vc clicka em um campo bandeira ele vira interrogacao
+                                elif(mina.hiden == Field.QUESTION):
+                                    count_flags -= 1
+                                    if(mina.number == Field.BOMB):
+                                        count_actual_flags -= 1
+
+                            # botao esquerdo
+                            elif(event.button == M_LCLICK):
+                                #mina.on_left_click()
+                                if(mina.number == Field.BOMB and mina.hiden == Field.DEFAULT):
+                                    perdeu = True
+                                    mine_field.revelar()
+                                    mina.set_bomb_exploded()
+                                mine_field.flood_fill(mina)
+        
+        if(count_actual_flags == NUM_MINES):
+            won = True
+
+        # ver quando preencher a tela toda toda com bandeira
         if(perdeu):
-            white = [255, 255 ,255]
-            red = [255, 0 ,0]
-            blue = [0, 0, 255]
-            global text
-            text = my_font.render("Tu perdeu", True, blue)
-    
+            screen.blit(PERDEU_TEXT, [WIDTH // 2 - PERDEU_TEXT.get_width() // 2, HEIGHT])
+        elif(won):
+            screen.blit(GANHOU_TEXT, [WIDTH // 2 - PERDEU_TEXT.get_width() // 2, HEIGHT])
+        else:
+            screen.fill([255, 255, 255])
         sprites.update()
         sprites.draw(screen)
-        screen.blit(text, [WIDTH // 2 - text.get_width() // 2, HEIGHT])
+
+        print(count_flags, count_actual_flags)
 
         pg.display.flip()
         clock.tick(FPS)
